99986b207c0b9f0f6c6adff3c26ec54443f2c6bf
Bernovich Denis <denisbernovich@gmail.com>
2023-01-22 13:08:47 +0300

    react_135

--- my-app/src/components/employees-list-item/employees-list-item.js
+++ my-app/src/components/employees-list-item/employees-list-item.js
@@ -23,7 +23,7 @@ addLike = () => {
 }
 
   render() {
-    const {name, salary} = this.props;
+    const {name, salary, onDelete} = this.props;
     const {increase, like} = this.state;
     let classNames = 'list-group-item d-flex justify-content-between'
   if (increase) {
@@ -37,24 +37,29 @@ addLike = () => {
 
   return (
     <li className={classNames}>
-      <span className="list-group-item-label"
-      onClick={this.addLike}
-      >{name}</span>
+      <span 
+      className="list-group-item-label"
+      onClick={this.addLike}>
+      {name}
+      </span>
       <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
       <div className='d-flex justify-content-center align-items-center'>
-        <button type="button"
+        <button 
+          type="button"
           className="btn-cookie btn-sm "
           onClick={this.onIncrease}>
           <i className="fas fa-cookie"></i>
         </button>
 
-        <button type="button"
-          className="btn-trash btn-sm ">
+        <button 
+          type="button"
+          className="btn-trash btn-sm "
+          onClick={onDelete}>
           <i className="fas fa-trash"></i>
         </button>
-          <i className="fas fa-star"></i>
+        <i className="fas fa-star"></i>
       </div>
-        </li>
+    </li>
   )
   }
 }
