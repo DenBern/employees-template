0d2a5c816a3c56e97bf8ad1da386e64a0b2b0592
Bernovich Denis <denisbernovich@gmail.com>
2023-01-22 19:37:22 +0300

    react_138

--- my-app/src/components/app-filter/app-filter.js
+++ my-app/src/components/app-filter/app-filter.js
@@ -1,23 +1,31 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+
+  const buttonsdata = [
+    {name: 'all', label: 'Все сотрудники'},
+    {name: 'rise', label: 'На повышение'},
+    {name: 'moreThen1000', label: 'З / П больше 1000 $'}
+  ];
+
+  const buttons = buttonsdata.map(({name, label}) => {
+    const active = props.filter === name;
+    const clazz = active ? 'btn-light' : 'btn-outline-light';
+    return (
+      <button 
+        className = {`btn ${clazz}`}
+        type = "button"
+        key={name}
+        onClick={() => props.onFilterSelect(name)}>
+        {label} 
+      </button>
+    )
+  })
+
   return ( 
-    <div className = "btn-group" >
-      <button className = "btn btn-light"
-      type = "button">
-      Все сотрудники 
-      </button>
-
-      <button className = "btn btn-outline-light"
-      type = "button"> 
-      На повышение 
-      </button>
-
-      <button className = "btn btn-outline-light"
-      type = "button">
-      З / П больше 1000 $ 
-      </button> 
-      </div>
+    <div className = "btn-group">
+      {buttons}
+    </div>
   );
 }
 
